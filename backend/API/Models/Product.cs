8bb76953261ab31cc6de7a5a188c5d8cf1154d71
domingasp <d.pleck@outlook.com>
2022-05-27 08:21:14 +0100

    backend restructure

---
+++ backend/API/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+public class Product : IEntity
+{
+    [Column("ProductID")]
+    public int Id { get; set; }
+
+    [MaxLength(50)]
+    public string Name { get; set; } = default!;
+}
